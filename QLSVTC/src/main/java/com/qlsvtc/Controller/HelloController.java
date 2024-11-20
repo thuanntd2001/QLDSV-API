a83b0517245311b5a94b2e69b8cbad552a6b6d60
thuanntd2001 <thiensudanduong12@gmail.com>
2024-11-20 17:17:08 +0700

    add jsp

---
+++ QLSVTC/src/main/java/com/qlsvtc/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.qlsvtc.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "World");
+        return "hello"; // This will resolve to /WEB-INF/jsp/hello.jsp
+    }
+}
