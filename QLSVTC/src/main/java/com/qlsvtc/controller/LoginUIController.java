96b0f757b1c7cb109a289c0ce769a7e5bddfde0a
thuanntd2001 <thiensudanduong12@gmail.com>
2024-11-27 15:19:56 +0700

    authorize fillter

--- QLSVTC/src/main/java/com/qlsvtc/controller/LoginUIController.java
+++ QLSVTC/src/main/java/com/qlsvtc/controller/LoginUIController.java
@@ -9,15 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.qlsvtc.dao.impl.DSPMDAO;
 import com.qlsvtc.dao.impl.NhanVienDAO;
@@ -25,7 +20,6 @@ import com.qlsvtc.model.DSPMModel;
 import com.qlsvtc.model.NhanVienLoginModel;
 import com.qlsvtc.model.UserModel;
 import com.qlsvtc.service.impl.CheckService;
-import com.qlsvtc.utils.FormUtil;
 import com.qlsvtc.utils.SessionUtil;
 
 @Controller
--- QLSVTC/src/main/java/com/qlsvtc/controller/XemBaoCaoKhoaController.java
+++ QLSVTC/src/main/java/com/qlsvtc/controller/XemBaoCaoKhoaController.java
@@ -1,9 +1,27 @@
 package com.qlsvtc.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.qlsvtc.dao.impl.BCBangDiemMonHocLTCDAO;
+import com.qlsvtc.dao.impl.BCDsLTCDAO;
+import com.qlsvtc.dao.impl.BCDssvDkLTCDAO;
+import com.qlsvtc.dao.impl.BCPhieuDiemDAO;
+import com.qlsvtc.model.baocao.BCBangDiemMonHocLTC;
+import com.qlsvtc.model.baocao.BCDsLTC;
+import com.qlsvtc.model.baocao.BCDssvDkLTC;
+import com.qlsvtc.model.baocao.BCPhieuDiem;
+import com.qlsvtc.model.para.ParaBCBangDiemMonHocLTC;
+import com.qlsvtc.model.para.ParaBCDsLTC;
+import com.qlsvtc.model.para.ParaPhieuDiem;
 
 @Controller
 @RequestMapping(value = "xembaocao")
@@ -26,5 +44,32 @@ public class XemBaoCaoKhoaController {
 		return "sv/Xembaocao";
 	}
 
+	BCBangDiemMonHocLTCDAO bcbangdiemmonhocltc = new BCBangDiemMonHocLTCDAO();
+	BCDsLTCDAO bcdsltc = new BCDsLTCDAO();
+	BCDssvDkLTCDAO bcdssvdkltc = new BCDssvDkLTCDAO();
+	BCPhieuDiemDAO bcphieudiem = new BCPhieuDiemDAO();
 	
+	@GetMapping("/bcbangdiemmonhocltc")
+	public List<BCBangDiemMonHocLTC> bcbangdiemmonhocltc(HttpSession session,ParaBCBangDiemMonHocLTC para) {
+		// tao dspm set ra view
+		return bcbangdiemmonhocltc.findAll(session,para.getNk(),para.getHk(),para.getMaMH(),para.getNhom());
+	}
+	
+	@GetMapping("/bcdsltc")
+	public List<BCDsLTC> bcdsltc(HttpSession session,ParaBCDsLTC para) {
+		// tao dspm set ra view
+		return bcdsltc.findAll(session,para.getNk(),para.getHk());
+	}
+	
+	@GetMapping("/bcdssvdkltc")
+	public List<BCDssvDkLTC> bcdssvdkltc(HttpSession session,ParaBCBangDiemMonHocLTC para) {
+		// tao dspm set ra view
+		return bcdssvdkltc.findAll(session,para.getNk(),para.getHk(),para.getMaMH(),para.getNhom());
+	}
+	
+	@GetMapping("/bcphieudiem")
+	public List<BCPhieuDiem> bcphieudiem(HttpSession session,ParaPhieuDiem para) {
+		// tao dspm set ra view
+		return bcphieudiem.findAll(session,para.getMaSV());
+	}
 }
